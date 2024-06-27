614d001f3769ddf3c1a9562876e811b9afe8d8cd
Leonardo Santos <leoleosantos21@gmail.com>
2024-06-27 19:02:34 -0300

    create: form-group em formsComponent

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
 
   results = [];
+  formGroup!: FormGroup;
 
+  constructor(private readonly formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+ 
+  onSubmit() {
+
+  }
+
+  onClean() {
+    this.formGroup.reset();
+  }
 }
